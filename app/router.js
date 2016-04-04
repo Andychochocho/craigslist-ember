f955045afe9b02403ddb8f3da940acabc0d7e65f
Andrew Cho and Timothy Durward <timothy.a.durward@gmail.com>
2016-04-04 16:05:06 -0700

    add house route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('apartment', { path: '/apartment/:apartment_id'});
+  this.route('house', { path: '/house/:house_id'});
 });
 
 export default Router;
