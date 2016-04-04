b50b86632e0e57b581b1b7e6f287c79aeb8cb033
Andrew Cho and Timothy Durward <cho-andrew@hotmail.com>
2016-04-04 16:01:32 -0700

    add path to apartment id route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('apartment');
+  this.route('apartment', { path: '/apartment/:apartment_id'});
 });
 
 export default Router;
